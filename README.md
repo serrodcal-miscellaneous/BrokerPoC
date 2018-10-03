10d3c06e7523e3787a3fbb34791e26f2b5ccd4d8
Sergio Rodríguez Calvo <sergiorodriguezcalvo@gmail.com>
2018-10-03 14:46:52 +0200

    Producer done without jar executable

--- README.md
+++ README.md
@@ -44,3 +44,54 @@ or
 ~$ kafkacat -C -b localhost:9092 -t mytopic
 ```
 
+## Producer
+
+Under `producer/` directory, there is a Maven project which uses Akka, Akka-Http, Akka-Stram, 
+Alpakka in Scala. This is a simple HTTP server which recives request by POST in `/publish` and 
+send the message in body to Kafka (topic name is "myTopic").
+
+Build this project as given below:
+
+```bash
+~$ mvn clean install
+```
+
+Run this server as given below:
+
+```bash
+~$ java -jar kafkaProducer-0.1.0-SNAPSHOT.jar
+```
+
+Open in other terminal window kafkacat as given below:
+```bash
+~$ kafkacat -C -b localhost:9092 -t myTopic
+   % Reached end of topic myTopic [0] at offset 0
+   
+```
+
+Send your request, for example:
+
+```bash
+~$ curl -i -X POST -d 'foo' 'http://localhost:8080/publish' 
+   HTTP/1.1 202 Accepted
+   Server: akka-http/10.1.5
+   Date: Wed, 03 Oct 2018 12:43:20 GMT
+   Content-Type: text/plain; charset=UTF-8
+   Content-Length: 34
+   
+   Message recived and sent to Kafka!%  
+```
+
+Finally, can see the output in kafkacat terminal window:
+
+```bash
+~$ kafkacat -C -b localhost:9092 -t myTopic
+   % Reached end of topic myTopic [0] at offset 0
+   f
+   o
+   o
+   % Reached end of topic myTopic [0] at offset 3
+```
+
+
+
---
+++ producer/src/main/scala/com/serrodcal/producer/Producer.scala
@@ -0,0 +1,74 @@
+package com.serrodcal.producer
+
+import akka.event.Logging
+import akka.stream.ActorMaterializer
+import akka.util.Timeout
+import com.typesafe.config.ConfigFactory
+import akka.http.scaladsl.server.Route
+
+import scala.io.StdIn
+import akka.http.scaladsl.model._
+import akka.http.scaladsl.server.Directives._
+import akka.http.scaladsl.Http
+import akka.kafka.ProducerSettings
+import akka.stream.scaladsl.Source
+import org.apache.kafka.common.serialization.StringSerializer
+import akka.actor.ActorSystem
+import org.apache.kafka.clients.producer.ProducerRecord
+
+import scala.concurrent.duration._
+
+/**
+ * @author serrodcal
+ */
+object Producer extends App {
+
+  val config = ConfigFactory.load()
+
+  val bootstrapServer = config.getString("akka.kafka.producer.kafka-clients.bootstrap.server")
+
+  val producerConfig = config.getConfig("akka.kafka.producer")
+
+  val producerSettings =
+    ProducerSettings(producerConfig, new StringSerializer, new StringSerializer)
+      .withBootstrapServers(bootstrapServer)
+
+  implicit val system = ActorSystem(config.getString("server.actor-system"))
+
+  implicit val materializer = ActorMaterializer()
+
+  // needed for the future flatMap/onComplete in the end
+  implicit val executionContext = system.dispatcher
+
+  implicit val logger = Logging(system, getClass)
+
+  implicit val timeout = Timeout(1 seconds)
+
+  val route : Route = post {
+    path("publish") {
+      entity(as[String]) { message =>
+        logger.info(s"Message recived: {$message}")
+        Source(message)
+          .map(_.toString)
+          .map(message => new ProducerRecord[String, String]("myTopic", message))
+          .runWith(akka.kafka.scaladsl.Producer.plainSink(producerSettings))
+        complete(StatusCodes.Accepted, "Message recived and sent to Kafka!")
+      }
+    }
+  }
+
+  // Pipe several routes: val routes = route1 ~ route2 ...
+  val routes = route
+
+  val host = config.getString("server.host")
+  val port = config.getInt("server.port")
+  val bindingFuture = Http().bindAndHandle(routes, host, port)
+
+  logger.info(s"Server online at http://$host:$port/\nPress RETURN to stop...")
+  StdIn.readLine() // let it run until user presses return
+  logger.info(s"Server stopped :(")
+  bindingFuture
+    .flatMap(_.unbind()) // trigger unbinding from the port
+    .onComplete(_ => system.terminate()) // and shutdown when done*/
+
+}

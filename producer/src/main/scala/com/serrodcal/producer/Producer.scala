b3df5e8e8e4d7f887cf7d4c5bf9b3432fc0e42a6
Sergio Rodríguez Calvo <sergiorodriguezcalvo@gmail.com>
2018-10-18 10:01:58 +0200

    new strategy using source.queue

--- producer/src/main/scala/com/serrodcal/producer/Producer.scala
+++ producer/src/main/scala/com/serrodcal/producer/Producer.scala
@@ -12,12 +12,20 @@ import akka.http.scaladsl.model._
 import akka.http.scaladsl.server.Directives._
 import akka.http.scaladsl.Http
 import akka.kafka.ProducerSettings
-import akka.stream.scaladsl.Source
+import akka.stream.scaladsl.{Sink, Source}
 import org.apache.kafka.common.serialization.StringSerializer
 import akka.actor.ActorSystem
 import org.apache.kafka.clients.producer.ProducerRecord
 
+import scala.concurrent.Future
 import scala.concurrent.duration._
+import akka.http.scaladsl.model.HttpMethods._
+import akka.http.scaladsl.unmarshalling.Unmarshal
+
+import scala.concurrent.Await
+import scala.util.{Failure, Success}
+
+import akka.actor.{Actor, ActorRef}
 
 /**
  * @author serrodcal
@@ -36,7 +44,7 @@ object Producer extends App {
 
   implicit val system = ActorSystem(config.getString("server.actor-system"))
 
-  implicit val materializer = ActorMaterializer()
+  implicit val materializer: ActorMaterializer = ActorMaterializer()
 
   // needed for the future flatMap/onComplete in the end
   implicit val executionContext = system.dispatcher
@@ -45,9 +53,51 @@ object Producer extends App {
 
   implicit val timeout = Timeout(1 seconds)
 
+  val host = config.getString("server.host")
+  val port = config.getInt("server.port")
+
   val topic = config.getString("akka.kafka.producer.kafka-clients.topic")
 
-  val route : Route = post {
+  val bufferSize = config.getInt("bufferSize")
+
+  //if the buffer fills up then this strategy drops the oldest elements
+  //upon the arrival of a new element.
+  val overflowStrategy = akka.stream.OverflowStrategy.dropHead
+
+  val queue = Source.queue[String](bufferSize, overflowStrategy)
+    .map(message => new ProducerRecord[String, String](topic, message))
+    .to(akka.kafka.scaladsl.Producer.plainSink(producerSettings))
+    .run()
+    //.to(Sink foreach println)
+    //.run() // in order to "keep" the queue Materialized value instead of the Sink's
+
+  val requestHandler: HttpRequest => HttpResponse = {
+    case HttpRequest(POST, Uri.Path("/publish"), _, entity, _) => {
+      val bodyMessage: Future[String] = Unmarshal(entity).to[String]
+      bodyMessage.onComplete{
+        case Success(message) => queue offer message
+        case Failure(_) => println("Some error getting message from body.")
+      }
+      HttpResponse(202, entity = "Message sent to Kafka!")
+    }
+    case r: HttpRequest =>
+      r.discardEntityBytes() // important to drain incoming HTTP Entity stream
+      HttpResponse(404, entity = "Unknown resource!")
+  }
+
+
+  val serverSource: Source[Http.IncomingConnection, Future[Http.ServerBinding]] =
+    Http().bind(interface = host, port = port)
+  val bindingFuture: Future[Http.ServerBinding] =
+    serverSource.to(Sink.foreach { connection =>
+        println("Accepted new connection from " + connection.remoteAddress)
+
+        connection handleWithSyncHandler requestHandler
+        // this is equivalent to
+        // connection handleWith { Flow[HttpRequest] map requestHandler }
+      }).run()
+
+  /*val route : Route = post {
     path("publish") {
       entity(as[String]) { message =>
         logger.info(s"Message recived: {$message}")
@@ -64,13 +114,13 @@ object Producer extends App {
 
   val host = config.getString("server.host")
   val port = config.getInt("server.port")
-  val bindingFuture = Http().bindAndHandle(routes, host, port)
+  val bindingFuture = Http().bindAndHandle(routes, host, port)*/
 
   logger.info(s"Server online at http://$host:$port/\nPress RETURN to stop...")
   StdIn.readLine() // let it run until user presses return
   logger.info(s"Server stopped :(")
   bindingFuture
     .flatMap(_.unbind()) // trigger unbinding from the port
-    .onComplete(_ => system.terminate()) // and shutdown when done*/
+    .onComplete(_ => system.terminate()) // and shutdown when done
 
 }
